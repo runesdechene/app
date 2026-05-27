021fd97ac8cfbf89e01cf8c5ffc8b41dc8a616af
UrielLahoussaye <uriel@lahoussaye.fr>
2026-05-27 22:24:15 +0200

    feat(hub): supprime le formulaire public d'avis (/soumettre-avis) - remplace par /soumettre-contenu

--- apps/hub/src/App.tsx
+++ apps/hub/src/App.tsx
@@ -6,7 +6,6 @@ import { Users } from './components/Users'
 import { UserDetail } from './components/UserDetail'
 import { Photos } from './components/Photos'
 import { StudioSubmit } from './components/StudioSubmit'
-import { ReviewSubmit } from './components/ReviewSubmit'
 import { TagsManager } from './components/TagsManager'
 import { Factions } from './components/Factions'
 import { TitlesManager } from './components/TitlesManager'
@@ -44,14 +43,13 @@ function App() {
   const location = useLocation()
 
   // Routes publiques (pas besoin d'auth)
-  const publicRoutes = ['/soumettre-contenu', '/soumettre-avis']
+  const publicRoutes = ['/soumettre-contenu']
   const isPublicRoute = publicRoutes.includes(location.pathname)
 
   if (isPublicRoute) {
     return (
       <Routes>
         <Route path="/soumettre-contenu" element={<StudioSubmit />} />
-        <Route path="/soumettre-avis" element={<ReviewSubmit />} />
       </Routes>
     )
   }
